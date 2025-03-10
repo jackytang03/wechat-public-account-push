c984fbcb07876aa3a7260f564e7fd75e6252fbdc
jackytang03 <86469410+jackytang03@users.noreply.github.com>
2025-03-10 23:50:25 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'obkr47LfeKSbTJg3JIRcyVB2MZuc',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'iX1WZO1KUPSjKTsvJgmfm8k9RPMeloyxTy6UPev9dMA',
+      useTemplateId: 'IpAYjah98XEqkAo4OrQ3xu4fJSkN50I03-dI7a9w2qs',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
