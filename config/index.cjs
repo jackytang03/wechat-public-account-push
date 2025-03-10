75c09a8ee86eb76bbc6d2e7b05c9fb7e54a1603f
jackytang03 <86469410+jackytang03@users.noreply.github.com>
2025-03-10 22:42:39 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -8,7 +8,7 @@
 const USER_CONFIG = {
 
  {"APP_ID":"
-wx93c2581be5a53dd7","APP_SECRET":"c7fea67f6d89a1b51d23c6eaef107650","IS_SHOW_COLOR":true,"CALLBACK_TEMPLATE_ID":"QDp1IZptx6sMNiTp_YMww7Wq5y7a-hyLDY4rXtaa4HU","CALLBACK_USERS":[{"name":"Tracer","id":"obkr47LfeKSbTJg3JIRcyVB2MZuc"}],"USERS":[{"name":"刘美","id":"********","useTemplateId":"QDp1IZptx6sMNiTp_YMww7Wq5y7a-hyLDY4rXtaa4HU","province":"四川","city":"德阳","horoscopeDate":"02-01","horoscopeDateType":"今日","openUrl":"https://shuangxunian.github.io/","festivals":[{"type":"*生日","name":"刘美","date":"09-01","year":"1997"},{"type":"*生日","name":"大飞","date":"01-12","year":"1997"}],"customizedDateList":[{"keyword":"恋爱纪念日","date":"2022-06-12"}]}],"SWITCH":{"weather":true,"holidaytts":true,"CIBA":true,"oneTalk":true,"earthyLoveWords":true,"momentCopyrighting":false,"poisonChickenSoup":true,"poetry":false,"horoscope":true,"birthdayMessage":true}}
+wx93c2581be5a53dd7","APP_SECRET":"c7fea67f6d89a1b51d23c6eaef107650","IS_SHOW_COLOR":true,"CALLBACK_TEMPLATE_ID":"QDp1IZptx6sMNiTp_YMww7Wq5y7a-hyLDY4rXtaa4HU","CALLBACK_USERS":[{"name":"Tracer","id":"obkr47LfeKSbTJg3JIRcyVB2MZuc"}],"USERS":[{"name":"刘美","id":"********","useTemplateId":"aG4HV3klu-_F3KvUT935V_AnwrNkrpBdUoRTuGZkx4k","province":"四川","city":"德阳","horoscopeDate":"02-01","horoscopeDateType":"今日","openUrl":"https://shuangxunian.github.io/","festivals":[{"type":"*生日","name":"刘美","date":"09-01","year":"1997"},{"type":"*生日","name":"大飞","date":"01-12","year":"1997"}],"customizedDateList":[{"keyword":"恋爱纪念日","date":"2022-06-12"}]}],"SWITCH":{"weather":true,"holidaytts":true,"CIBA":true,"oneTalk":true,"earthyLoveWords":true,"momentCopyrighting":false,"poisonChickenSoup":true,"poetry":false,"horoscope":true,"birthdayMessage":true}}
 
 
 }
