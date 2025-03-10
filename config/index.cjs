e897cdb4c06044c9fc7f339376f230c142ac2796
jackytang03 <86469410+jackytang03@users.noreply.github.com>
2025-03-10 22:51:32 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '刘美',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'obkr47LfeKSbTJg3JIRcyVB2MZuc',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'aG4HV3klu-_F3KvUT935V_AnwrNkrpBdUoRTuGZkx4k',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
